0ff78a8c19991aaf43a0b1db65ed265269ac89a6
snikdhendu <snikdhendupramanik@gmail.com>
2024-09-07 01:10:19 +0530

    User model added

---
+++ server/src/models/User.ts
@@ -0,0 +1,37 @@
+import { Document, model, Schema, Types } from 'mongoose';
+
+interface IUser extends Document {
+    clerkUserId: string;
+    firstName: string;
+    lastName?: string;
+    dob?: string;
+    gender?:string;
+    fatherName?:string;
+    location?:string;
+    mobileNumber?:string;
+    year?: string;
+    profileUrl?: string;
+    email: string;
+    collegeName?: string;
+
+     
+  }
+const UserSchema=new Schema<IUser>({
+    clerkUserId: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, default: '' },
+    dob:{type:String,default:''},
+    gender:{type:String,default:'',required:true},
+    fatherName:{type:String,default:'',required:true},
+    location:{type:String,default:'',required:true},
+    mobileNumber:{type:String,default:''},
+    year:{type:String,default:''},
+    email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ },
+    collegeName: { type: String, default: '' },
+
+},{
+    timestamps: true,
+})
+
+export const User = model<IUser>('User', UserSchema);
+export type{IUser};
