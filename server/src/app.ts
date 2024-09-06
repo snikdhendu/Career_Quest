bf2ffe68a8072957f9da3403c019f9c4c84ea5a2
Sounab Bhattacharjee <sounabbhattacharjee@gmail.com>
2024-09-06 23:28:02 +0530

    socket frontend setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,6 +19,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
+    credentials: true,
     origin: "http://localhost:5173",  // Adjust according to your frontend URL
     methods: ["GET", "POST"]
   }
