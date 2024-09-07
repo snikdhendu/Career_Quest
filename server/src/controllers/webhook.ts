9ecbdad26d6b2e3fb0fdc1e4527fdf263e1a6a6b
Shreyam Kundu <shreyamkundu04@gmail.com>
2024-09-07 11:36:54 +0530

    webhook setup

--- server/src/controllers/webhook.ts
+++ server/src/controllers/webhook.ts
@@ -1,57 +1,58 @@
-// import { Request, Response } from "express";
-// import { Webhook } from "svix";
-// import { User } from "../models/User.js";
+import { Request, Response } from "express";
+import { Webhook } from "svix";
+import { User } from "../models/User.js";
 
-// export const handleWebhook = async (req: Request, res: Response) => {
-//   try {
-//     const payloadString = req.body.toString("utf8");
 
-//     const svixHeaders: any = {
-//       "svix-id": req.headers["svix-id"] as string,
-//       "svix-timestamp": req.headers["svix-timestamp"] as string,
-//       "svix-signature": req.headers["svix-signature"] as string,
-//     };
+export const handleWebhook = async (req: Request, res: Response) => {
+  try {
+    const payloadString = req.body.toString("utf8");
 
-//     const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY!);
+    const svixHeaders: any = {
+      "svix-id": req.headers["svix-id"] as string,
+      "svix-timestamp": req.headers["svix-timestamp"] as string,
+      "svix-signature": req.headers["svix-signature"] as string,
+    };
 
-//     const evt: any = wh.verify(payloadString, svixHeaders);
-//     const { id, ...attributes } = evt.data;
+    const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET_KEY!);
 
-//     // Handle the webhooks
-//     const eventType = evt.type;
-//     if (eventType === "user.created") {
-//       console.log(`User ${id} was ${eventType}`);
+    const evt: any = wh.verify(payloadString, svixHeaders);
+    const { id, ...attributes } = evt.data;
 
-//       console.log(attributes);
+    // Handle the webhooks
+    const eventType = evt.type;
+    if (eventType === "user.created") {
+      console.log(`User ${id} was ${eventType}`);
 
-//       const firstName = attributes.first_name;
-//       const lastName = attributes.last_name;
-//       const email = attributes.email_addresses[0].email_address;
-//       const profileUrl = attributes.profile_image_url;
+      console.log(attributes);
 
-//       const user = new User({
-//         clerkUserId: id,
-//         firstName: firstName,
-//         lastName: lastName,
-//         email: email,
-//         profileUrl: profileUrl,
-//       });
+      const firstName = attributes.first_name;
+      const lastName = attributes.last_name;
+      const email = attributes.email_addresses[0].email_address;
+      const profileUrl = attributes.profile_image_url;
 
-//       await user.save();
-//       console.log("User saved to database");
-//     }
+      const user = new User({
+        clerkUserId: id,
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        profileUrl: profileUrl,
+      });
 
-//     res.status(200).json({
-//       success: true,
-//       message: "Webhook received",
-//     });
-//   } catch (err: any) {
-//     console.error("Error handling webhook:", err);
-//     console.error("Headers:", req.headers);
-//     console.error("Payload:", req.body.toString("utf8"));
-//     res.status(400).json({
-//       success: false,
-//       message: err.message,
-//     });
-//   }
-// };
+      await user.save();
+      console.log("User saved to database");
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Webhook received",
+    });
+  } catch (err: any) {
+    console.error("Error handling webhook:", err);
+    console.error("Headers:", req.headers);
+    console.error("Payload:", req.body.toString("utf8"));
+    res.status(400).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
